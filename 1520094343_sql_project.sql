e798af6e750584710c961b365f840b3d234c5a8a
caojingyu <53276776+caojingyu@users.noreply.github.com>
2019-09-08 02:20:43 -0400

    Update 1520094343_sql_project.sql

--- 1520094343_sql_project.sql
+++ 1520094343_sql_project.sql
@@ -27,8 +27,15 @@ exploring the data, and getting acquainted with the 3 tables. */
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Please list the names of the facilities that do. */
 
+SELECT facid, name, membercost
+FROM Facilities
+WHERE membercost = 0
+
 
 /* Q2: How many facilities do not charge a fee to members? */
+
+SELECT COUNT(CASE WHEN membercost = 0.0 THEN 1 ELSE NULL END) AS free
+FROM Facilities
 
 
 /* Q3: How can you produce a list of facilities that charge a fee to members,
@@ -36,9 +43,17 @@ where the fee is less than 20% of the facility's monthly maintenance cost?
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
 
+SELECT facid, name, membercost, monthlymaintenance
+FROM Facilities
+WHERE membercost > 0.0 AND membercost < 0.2*monthlymaintenance
+
 
 /* Q4: How can you retrieve the details of facilities with ID 1 and 5?
 Write the query without using the OR operator. */
+
+SELECT *
+FROM Facilities
+WHERE facid IN (1, 5)
 
 
 /* Q5: How can you produce a list of facilities, with each labelled as
@@ -46,15 +61,32 @@ Write the query without using the OR operator. */
 more than $100? Return the name and monthly maintenance of the facilities
 in question. */
 
+SELECT name, monthlymaintenance, CASE WHEN monthlymaintenance > 100 THEN 'expensive' ELSE 'cheap' END AS cheapexpensive
+FROM Facilities
+
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Do not use the LIMIT clause for your solution. */
+
+SELECT firstname, surname, joindate
+FROM Members
+ORDER BY joindate DESC
 
 
 /* Q7: How can you produce a list of all members who have used a tennis court?
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
+
+SELECT Bookings.memid, CONCAT(Members.firstname, ' ', Members.surname) AS fullname, Bookings.facid, Facilities.name
+FROM Bookings
+JOIN Facilities
+ON Facilities.facid = Bookings.facid
+JOIN Members
+ON Members.memid = Bookings.memid
+WHERE Facilities.name LIKE 'Tennis Court%'
+GROUP BY fullname
+ORDER BY fullname
 
 
 /* Q8: How can you produce a list of bookings on the day of 2012-09-14 which
