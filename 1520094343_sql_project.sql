e45a92106d1993d69e49f717b7bb0c3633b766f5
caojingyu <53276776+caojingyu@users.noreply.github.com>
2019-09-08 17:10:28 -0400

    Update 1520094343_sql_project.sql

--- 1520094343_sql_project.sql
+++ 1520094343_sql_project.sql
@@ -96,12 +96,6 @@ the guest user's ID is always 0. Include in your output the name of the
 facility, the name of the member formatted as a single column, and the cost.
 Order by descending cost, and do not use any subqueries. */
 
-/* Q8: How can you produce a list of bookings on the day of 2012-09-14 which
-will cost the member (or guest) more than $30? Remember that guests have
-different costs to members (the listed costs are per half-hour 'slot'), and
-the guest user's ID is always 0. Include in your output the name of the
-facility, the name of the member formatted as a single column, and the cost.
-Order by descending cost, and do not use any subqueries. */
 SELECT Bookings.starttime, Bookings.memid, CONCAT(Members.firstname, ' ', Members.surname) AS fullname, Bookings.facid, Facilities.name, CASE WHEN Bookings.memid = '0' THEN Bookings.slots * Facilities.guestcost ELSE Bookings.slots * Facilities.membercost END AS cost
 FROM Bookings
 JOIN Facilities
@@ -110,7 +104,7 @@ JOIN Members
 ON Members.memid = Bookings.memid
 Where Bookings.starttime LIKE '2012-09-14%'
 AND (CASE WHEN Bookings.memid = '0' THEN Bookings.slots * Facilities.guestcost ELSE Bookings.slots * Facilities.membercost END) > 30.0
-ORDER BY fullname
+ORDER BY cost DESC
 
 
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
